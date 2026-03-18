3df4b8b259a976d85633f2858340e416c971395f
Owen Andrews <owen@owenandre.ws>
2026-03-18 16:36:04 +1100

    Tests WIP

---
+++ tests/csv2sql/test_csv2sql.py
@@ -0,0 +1,24 @@
+# Planning what tests are needed
+def test_insert_includes_expected_fields():
+    pass
+
+def test_insert_can_skip_fields():
+    pass
+
+def test_update_includes_expected_fields():
+    pass
+
+def test_update_excludes_null_fields():
+    pass
+
+def test_update_includes_null_fields_explicitly():
+    pass
+
+def test_insert_with_header_on_specified_row():
+    pass
+
+def test_can_read_xlsx():
+    pass
+
+def test_can_read_named_sheet():
+    pass
